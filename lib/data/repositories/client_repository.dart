0b5f072e78a2aa36823a45c5cc6757d594c364a7
Sonats <ti@raizhe.com.br>
2024-10-16 14:08:38 -0300

    CODE UPDATED

--- lib/data/repositories/client_repository.dart
+++ lib/data/repositories/client_repository.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/client_model.dart';
 
-
 class ClientRepository {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
